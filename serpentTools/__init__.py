b41722c0597872baf17d6b7878a3f4b3479f3f90
Andrew Johnson <1drew.e.johnson@gmail.com>
2017-09-18 14:41:54 -0400

    Quick fix: DepletedMaterial getXY only returns time if no time is given as input
    Signed-off-by: Andrew Johnson <1drew.e.johnson@gmail.com>

--- serpentTools/__init__.py
+++ serpentTools/__init__.py
@@ -1,7 +1,7 @@
 from serpentTools import settings
 from serpentTools import parsers
 
-__version__ = '0.1.0'
+__version__ = '0.1.1rc0'
 
 # List TODOS/feature requests here for now
 # Messages/Errors
@@ -11,9 +11,6 @@ __version__ = '0.1.0'
 # Compatability
 # TODO: Python 2 support
 # TODO: Test compatability with earlier numpy releases
-# Pythonicness/cleanliness
-# TODO: Better rc importing and usage
-# TODO: Remove need to specify all settings as a default setting object
 # Usage/scripting
 # TODO: Update rc with dictionary
 # TODO: Update rc with yaml file into dictionary
