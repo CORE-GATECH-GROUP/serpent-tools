49fdf715e85da6b18eeb9940da0ac3fc734b659d
Andrew Johnson <1drew.e.johnson@gmail.com>
2017-10-05 15:53:13 -0400

    Removed some stuff in the TODO block relating to this update
    Signed-off-by: Andrew Johnson <1drew.e.johnson@gmail.com>

--- serpentTools/__init__.py
+++ serpentTools/__init__.py
@@ -3,10 +3,6 @@ from serpentTools import parsers
 
 
 # List TODOS/feature requests here for now
-# Messages/Errors
-# TODO: Add verbosity control
-# TODO: Add specific exceptions and warnings
-# TODO: Add logging module to help with warnings/exceptions/info
 # Compatability
 # TODO: Python 2 support
 # TODO: Test compatability with earlier numpy releases
@@ -14,7 +10,6 @@ from serpentTools import parsers
 # TODO: Update rc with dictionary
 # TODO: Update rc with yaml file into dictionary
 # TODO: Capture materials with underscores for depletion
-# TODO: Better version string management
 
 
 from ._version import get_versions
