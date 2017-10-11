641c408aaeef2cdced67a81ade9809be798e1102
Unknown <dan.kotlyar@me.gatech.edu>
2017-10-11 08:26:02 -0400

    Read results
    Wrok in progress

--- serpentTools/parsers/results.py
+++ serpentTools/parsers/results.py
@@ -1,7 +1,13 @@
 """Parser responsible for reading the ``*res.m`` files"""
 
 
+import re
+
+from drewtils.parsers import KeywordParser
+
 from serpentTools.objects.readers import XSReader
+
+from serpentTools.settings import messages
 
 
 class ResultsReader(XSReader):
@@ -15,3 +21,19 @@ class ResultsReader(XSReader):
     """
     def __init__(self, filePath):
         XSReader.__init__(self, filePath, ['branching', 'xs'])
+
+    def read(self):
+        """Read through the depletion file and store requested data."""
+        messages.info('Preparing to read {}'.format(self.filePath))
+        keys = ['MAT', 'TOT'] if self.settings['processTotal'] else ['MAT']
+        keys.extend(self.settings['metadataKeys'])
+        separators = ['\n', '];']
+        with KeywordParser(self.filePath, keys, separators) as parser:
+            for chunk in parser.yieldChunks():
+                if 'MAT' not in chunk[0] and 'TOT' not in chunk[0]:
+                    self._addMetadata(chunk)
+                elif (('TOT' in chunk[0] and self.settings['processTotal'])
+                      or 'MAT' in chunk[0]):
+                    self._addMaterial(chunk)
+        messages.info('Done reading depletion file')
+        messages.debug('  found {} materials'.format(len(self.materials)))
