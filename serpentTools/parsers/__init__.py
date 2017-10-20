d56220958cecedc4b413d56471874c64c1159999
Unknown <stefano.st.terlizzi@gmail.com>
2017-10-20 11:40:45 -0400

    Required changes

--- serpentTools/parsers/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Package dedicated for reading ``SERPENT`` output files"""
-from serpentTools.parsers.depletion import DepletionReader
-from serpentTools.parsers.branching import BranchingReader
--- serpentTools/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-import os
-
-TEST_ROOT = os.path.dirname(__file__)
