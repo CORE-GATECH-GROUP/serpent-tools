502d1a360798ed06044f97dee5d86b955f65a0f6
sallustius <stefano.st.terlizzi@gmail.com>
2017-10-20 11:58:59 -0400

    Revert "Required changes"
    This reverts commit d56220958cecedc4b413d56471874c64c1159999.

---
+++ serpentTools/parsers/results.py
@@ -0,0 +1,15 @@
+"""Parser responsible for reading the ``*res.m`` files"""
+
+from serpentTools.objects.readers import BaseReader
+
+
+class ResultsReader(BaseReader):
+    """
+    Parser responsible for reading and working with result files.
+
+    Parameters
+    ----------
+    filePath: str
+        path to the depletion file
+    """
+    pass
