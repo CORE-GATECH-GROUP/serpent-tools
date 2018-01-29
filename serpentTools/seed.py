6bf64c0161e330531cf02873212dc08f286c7050
andrewjohnson <ajohnson400@gatech.edu>
2018-01-29 10:59:08 -0500

    Function to create multiple files using unique seeds
    Allows the user to specify number of files to create, starting seed for builtin RNG, output directory, and a rewrite option. If True, the entire input file is copied as a new file into the output directory. If False, the new file uses the include functionality to link the original file to the new file. This results in very short files, only containing the include line and the new seed

---
+++ serpentTools/seed.py
@@ -0,0 +1,99 @@
+"""
+Function to copy an input file N times with N randomly generated seeds
+"""
+
+import os
+from os import path
+from shutil import copy
+import random
+
+from six.moves import range
+
+from serpentTools.messages import critical, debug
+
+
+def _writeSeed(stream):
+    stream.write('\nset seed {}'.format(random.getrandbits(32)))
+
+
+def _makeFileFmt(inputFile):
+    baseName = path.basename(inputFile)
+    if '.' in baseName:
+        s = baseName.split('.')
+        base = s[0]
+        ext = '.' + '.'.join(s[1:])
+    else:
+        base = baseName
+        ext = ''
+    return base + '_{}' + ext
+
+
+def _include(inputFile, numSeeds, fileFmt):
+    for N in range(numSeeds):
+        name = fileFmt.format(N)
+        with open(name, 'w') as stream:
+            stream.write('include \"{}\"\n'.format(inputFile))
+            _writeSeed(stream)
+
+
+def _copy(inputFile, numSeeds, fileFmt):
+    for N in range(numSeeds):
+        name = fileFmt.format(N)
+        copy(inputFile, name)
+        with open(name, 'a') as stream:
+            _writeSeed(stream)
+
+
+def seedFiles(inputFile, numSeeds, seed=None, outputDir=None, rewrite=True):
+    """
+    Copy input file multiple times with unique seeds.
+
+    Parameters
+    ----------
+    inputFile: str
+        Path to input file
+    numSeeds: int
+        Number of files to create
+    seed: int
+        Optional argument to set the seed of the builtin random
+        number generator
+    outputDir: str
+        Path to desired output directory. Files will be copied here.
+        If the folder does not exist, try to make the directory. Assumes path
+        relative to directory that contains the input file
+    rewrite: bool
+        If False, do not copy the full file. Instead, create a new file
+        with 'include <inputFile>' and the new seed declaration.
+
+    See Also
+    --------
+    :py:mod:`random`
+    :py:func:`random.seed()`
+
+    """
+    if '~' in inputFile:
+        inputFile = os.path.expanduser(inputFile)
+
+    if not path.exists(inputFile):
+        critical('Input file {} does not exist'.format(inputFile))
+        return
+
+    random.seed(seed)
+
+    inputPath = path.abspath(path.join(os.getcwd(), inputFile))
+    inputRoot = path.dirname(inputPath)
+
+    if outputDir is not None:
+        fPrefix = path.abspath(path.join(inputRoot, outputDir))
+        if not path.isdir(fPrefix):
+            debug('Creating directory at {}'.format(fPrefix))
+            os.mkdir(fPrefix)
+    else:
+        fPrefix = inputRoot
+
+    fileFmt = path.join(fPrefix, _makeFileFmt(inputFile))
+
+    writeFunc = _copy if rewrite else _include
+    writeFunc(inputPath, numSeeds, fileFmt)
+
+    return
