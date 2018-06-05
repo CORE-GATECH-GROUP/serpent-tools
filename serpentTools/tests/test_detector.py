23a8d0925c17c3944c98909e79d4f8d569e2ac0d
Andrew Johnson <drewej@protonmail.com>
2018-06-04 23:19:55 -0400

    TST Abstract testing off DetectorTester onto Helper
    In the process of closing #184, many detector objects
    will be written, each with their own very similar but
    different grid layout, tally data, and indices.
    Working on setting up sub-classed Testers in
    serpentTools/tests/test_detector.py that have the
    expected data structured on the class object, e.g.
    
    class Helper(TestCase):
        def test_grids(self):
            <do something with self.EXPECTED_GRIDS>
    class Tester(Helper):
        EXPECTED_GRIDS = {
            ...
        }
    
    This method also **should** read the unique files for
    hexagonal and curvilinear detector files in the SetUpClass
    method for each subclass.
    
    Eventually, a final test will be made where all the files
    are joined together to demonstrate that the detector reader
    can handle a file with multiple output types.

--- serpentTools/tests/test_detector.py
+++ serpentTools/tests/test_detector.py
@@ -1,17 +1,72 @@
 """Test the detector reader."""
 
 import os
-import unittest
+from unittest import TestCase
+
 from collections import OrderedDict
 
-import numpy
-from numpy.testing import assert_allclose, assert_equal
+from six import iteritems
+from numpy import arange, array
+from numpy.testing import assert_equal
 
 from serpentTools.parsers import DetectorReader
-from serpentTools.tests import TEST_ROOT
+from serpentTools.objects.detectors import CartesianDetector
+from serpentTools.tests import TEST_ROOT, compareDictOfArrays
 
 
-class DetectorReaderTester(unittest.TestCase):
+def read(fileP):
+    reader = DetectorReader(fileP)
+    reader.read()
+    return reader
+
+
+class DetectorHelper(TestCase):
+    """
+    Class that assists setting up readers
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        cls.reader = read(cls.FILE_PATH)
+        cls.detectors = cls.reader.detectors
+
+    def test_loadedDetectors(self):
+        """Verify that all anticipated detectors are loaded"""
+        expectedNames = set(self.EXPECTED_DETECTORS.keys())
+        actualNames = set(self.reader.detectors.keys())
+        self.assertSetEqual(
+            expectedNames, actualNames,
+            msg="Failure reading detectors from {}".format(self.FILE_PATH))
+        for name, cls in iteritems(self.EXPECTED_DETECTORS):
+            actualDet = self.detectors[name]
+            self.assertIsInstance(
+                    actualDet, cls, msg="{} is {}, should be {}: - {}"
+                    .format(name, actualDet.__class__.__name__, cls,
+                            self.FILE_PATH))
+
+    def test_detectorGrids(self):
+        """Verify that all grids are loaded"""
+        baseMsg = "Key: {key}"
+        for detName, gridDict in iteritems(self.EXPECTED_GRIDS):
+            msg = baseMsg + "  Reading: " + self.__class__.__name__
+            actualGrids = self.detectors[detName].grids
+            compareDictOfArrays(
+                gridDict, actualGrids, testCase=self, fmtMsg=msg)
+
+    def test_detectorIndex(self):
+        """Verify that the detector tally index is properly constructed"""
+        for detName, expectedIndex in iteritems(self.EXPECTED_INDEXES):
+            actualIndex = self.detectors[detName].indexes
+            expectedKeys = list(expectedIndex.keys())
+            actualKeys = list(actualIndex.keys())
+            self.assertListEqual(actualKeys, expectedKeys)
+            for key in expectedIndex:
+                assert_equal(
+                    actualIndex[key], expectedIndex[key],
+                    err_msg="Key: {}, Detector: {}".format(key, detName))
+
+
+class CartesianDetectorReaderTester(DetectorHelper):
     """
     Class to test the detector reader.
 
@@ -21,85 +76,61 @@ class DetectorReaderTester(unittest.TestCase):
            reactions: U-235 fission and capture
     """
 
-    @classmethod
-    def setUpClass(cls):
-        cls.file = os.path.join(TEST_ROOT, 'ref_det0.m')
-        cls.reader = DetectorReader(cls.file)
-        cls.reader.read()
-        expected = {'xyFissionCapt'}
-        actual = set(cls.reader.detectors.keys())
-        diff = expected.symmetric_difference(actual)
-        assert not any(diff), (
-            'Failed to load detector correctly. '
-            'Incorrect detectors: {}'.format(diff)
-        )
-        cls.refDet = cls.reader.detectors['xyFissionCapt']
-        expectedIndexKeys = {'xmesh', 'ymesh', 'reaction'}
-        diffIndexK = expectedIndexKeys.symmetric_difference(
-            cls.refDet.indexes.keys())
-        assert not any(diffIndexK), (
-            'Unexpected index keys for detector {}: {}'.format(cls.refDet.name,
-                                                               diffIndexK)
-        )
-        expectedGrids = {'X', 'Y', 'Z'}
-        diffGrids = expectedGrids.symmetric_difference(cls.refDet.grids.keys())
-        assert not any(diffGrids), (
-            'Unexpected grid keys for detector {}: {}'.format(cls.refDet.name,
-                                                              diffGrids)
-        )
+    __name__ = "Cartesian"
 
-    def test_detectorGrids(self):
-        """Verify that the detector grids are property constructed"""
-        expectedXY = numpy.array([
+    FILE_PATH = os.path.join(TEST_ROOT, 'ref_det0.m')
+    DET_NAME = 'xyFissionCapt'
+    EXPECTED_DETECTORS = {
+        DET_NAME: CartesianDetector
+    }
+    _EXPECTED_GRIDS = {
+        'X': array([
             [-1.95000E+00, - 1.17000E+00, - 1.56000E+00],
             [- 1.17000E+00, - 3.90000E-01, - 7.80000E-01],
             [- 3.90000E-01, 3.90000E-01, 2.22045E-16],
             [3.90000E-01, 1.17000E+00, 7.80000E-01],
             [1.17000E+00, 1.95000E+00, 1.56000E+00]
+        ]),
+        'Z': array([[-1.00000E+37, 1.00000E+37, 0.00000E+00]])
+    }
+    _EXPECTED_GRIDS['Y'] = _EXPECTED_GRIDS['X']
+    EXPECTED_GRIDS = {
+        DET_NAME: _EXPECTED_GRIDS
+    }
+    _INDEXES = OrderedDict([
+        ['reaction', arange(2)],
+        ['ymesh', arange(5)],
+        ['xmesh', arange(5)],
         ])
-        expectedZ = numpy.array([[-1.00000E+37, 1.00000E+37, 0.00000E+00]])
-        assert_allclose(self.refDet.grids['X'], expectedXY,
-                        err_msg='x grid incorrect')
-        assert_allclose(self.refDet.grids['Y'], expectedXY,
-                        err_msg='y grid incorrect')
-        assert_allclose(self.refDet.grids['Z'], expectedZ,
-                        err_msg='z grid incorrect')
-
-    def test_detectorIndex(self):
-        """Verify that the detector tally index is properly constructed"""
-        expected = OrderedDict()
-        expected['reaction'] = numpy.array([0, 1])
-        expected['ymesh'] = numpy.array([0, 1, 2, 3, 4])
-        expected['xmesh'] = numpy.array([0, 1, 2, 3, 4])
-        expectedKeys = list(expected.keys())
-        actualIndex = self.refDet.indexes
-        actualKeys = list(actualIndex.keys())
-        self.assertListEqual(actualKeys, expectedKeys)
-        for key in expected:
-            assert_equal(actualIndex[key], expected[key])
+    EXPECTED_INDEXES = {DET_NAME: _INDEXES}
 
     def test_detectorSlice(self):
         """Verify the slicing method"""
+        refDet = self.detectors[self.DET_NAME]
         constrain = {'reaction': 0}
-        expectedTallies = numpy.array([
+        expectedTallies = array([
             [2.55119E-01, 2.55077E-01, 2.53685E-01, 2.55592E-01, 2.58450E-01],
             [2.54101E-01, 2.53408E-01, 2.56666E-01, 2.55375E-01, 2.52936E-01],
             [2.56006E-01, 2.51002E-01, 2.55479E-01, 2.52002E-01, 2.54708E-01],
             [2.54957E-01, 2.53399E-01, 2.48180E-01, 2.52915E-01, 2.53914E-01],
             [2.58394E-01, 2.50217E-01, 2.59642E-01, 2.54025E-01, 2.57076E-01]
         ])
-        assert_equal(self.refDet.slice(constrain), expectedTallies,
+        assert_equal(refDet.slice(constrain), expectedTallies,
                      err_msg='error in expected tally slice')
-        expectedErrors = numpy.array([
+        expectedErrors = array([
             [0.01445, 0.01063, 0.01190, 0.01193, 0.01334],
             [0.01006, 0.00916, 0.01240, 0.00933, 0.01002],
             [0.01317, 0.01187, 0.01386, 0.01120, 0.01171],
             [0.01081, 0.00885, 0.01127, 0.00893, 0.01161],
             [0.01250, 0.01121, 0.01460, 0.01142, 0.01219]
         ])
-        assert_equal(self.refDet.slice(constrain, data='errors'),
+        assert_equal(refDet.slice(constrain, data='errors'),
                      expectedErrors, err_msg='error in expected error slice')
 
 
+del DetectorHelper
+
+
 if __name__ == '__main__':
-    unittest.main()
+    from unittest import main
+    main()
