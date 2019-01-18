f3f12bdc3b3cfad4f00c7772c269222a476a5176
Andrew Johnson <ajohnson400@gatech.edu>
2019-01-18 10:16:25 -0500

    DEV s/u/hasScipy -> s/u/checkScipy
    Better function for version checking. Will be used in
    later converting methods to ensure that the user has
    the correct library version installed.
    
    At this point, we don't want to include lots of additional
    packages as requirements, so we'll do this version checking

--- serpentTools/utils/__init__.py
+++ serpentTools/utils/__init__.py
@@ -1,18 +1,19 @@
 """
 Commonly used functions and utilities
 """
-try:
-    import scipy  # noqa
-    __HAS_SCIPY = True
-except ImportError:
-    __HAS_SCIPY = False
+from distutils.version import StrictVersion
 
 from serpentTools.utils.core import *  # noqa
 from serpentTools.utils.docstrings import *  # noqa
 from serpentTools.utils.compare import *  # noqa
 from serpentTools.utils.plot import *  # noqa
 
-
-def hasScipy():
-    """Return ``True`` if :term:`scipy` is installed."""
-    return __HAS_SCIPY is True
+def checkScipy(version=None):
+    """Return ``True`` if the given version of :term:`scipy` is installed"""
+    try:
+        import scipy
+    except ImportError:
+        return False
+    if version is None:
+        return True
+    return StrictVersion(scipy.__version__) >= StrictVersion(version)
