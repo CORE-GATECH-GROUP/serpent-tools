dead4de72a1a9eadfb78d467637a654de2fd37ab
Andrew Johnson <ajohnson400@gatech.edu>
2019-11-12 13:02:31 -0500

    BUG Fix detector.names setting
    The detector.names setting is supposed to contain a list
    of detectors to be stored on the reader. Instead, it previously
    read in detectors that were not in the list, meaning it worked
    more like anti-pattern. This has been remedied.

--- serpentTools/parsers/detector.py
+++ serpentTools/parsers/detector.py
@@ -76,7 +76,7 @@ class DetectorReader(BaseReader):
 
     def _processDet(self, name, bins, grids):
         """Add this detector with it's grid data to the reader."""
-        if self.settings['names'] and name in self.settings['names']:
+        if self.settings['names'] and name not in self.settings['names']:
             return
         if name in self.detectors:
             raise KeyError("Detector {} already stored on reader"
