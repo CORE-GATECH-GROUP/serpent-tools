6cda97be795050af925bdda6fd484717b61b13d8
Andrew Johnson <ajohnson400@gatech.edu>
2020-02-19 13:35:41 -0500

    DOC Fix documentation for matlab hook

--- serpentTools/io/hooks.py
+++ serpentTools/io/hooks.py
@@ -37,31 +37,31 @@ def matlabHook(obj, fileP, reconvert=True, append=True,
 
     Parameters
     ----------
-    fileP: str or file-like object
+    fileP : str or file-like object
         Name of the file to write. ``.mat`` extension is not needed
         if ``append==True``
-    reconvert: bool
+    reconvert : bool
         If this evaluates to true, convert values back into their
         original form as they appear in the output file.
-    append: bool
+    append : bool
         If true and a file exists under ``output``, append to that file.
         Otherwise the file will be overwritten
-    format: {'5', '4'}
+    format : {'5', '4'}
         Format of file to write. ``'5'`` for MATLAB 5 to 7.2, ``'4'`` for
         MATLAB 4
-    longNames: bool
+    longNames : bool
         If true, allow variable names to reach 63 characters,
         which works with MATLAB 7.6+. Otherwise, maximum length is
         31 characters
-    compress: bool
+    compress : bool
         If true, compress matrices on write
-    oned: {'row', 'col'}:
+    oned : {'row', 'col'}
         Write one-dimensional arrays as row vectors if
         ``oned=='row'`` (default), or column vectors
 
     Raises
     ------
-    ImportError:
+    ImportError
         If :term:`scipy` is not installed
 
     See Also
