27de174cd261f4810256cbd4496338baa1b4fa6c
Andrew Johnson <drewej@protonmail.com>
2020-05-04 09:13:17 -0400

    TST Add pytest specific XSPlot tester
    Tests dictionary access, plot labeling, and describe method

--- tests/test_pt_xsplot.py
+++ tests/test_pt_xsplot.py
@@ -1,8 +1,11 @@
 """Pytests for the xsplot reader"""
 
 
+import numpy
 import pytest
 import serpentTools
+
+from . import getLegendTexts
 
 
 @pytest.fixture(scope="module")
@@ -31,3 +34,42 @@ def test_dictaccess(plut):
     assert actual == expected
 
     assert plut.get("this should not exist") is None
+
+
+@pytest.mark.parametrize("name", ["i8016_03c", "mfissile"])
+def test_mts(plut, name):
+    xsd = plut[name]
+    for i, (mt, desc) in enumerate(zip(xsd.MT, xsd.MTdescrip)):
+        assert numpy.equal(xsd[mt], xsd.xsdata[:, i]).all()
+        assert xsd.describe(mt) == desc
+
+
+@pytest.mark.parametrize("name", ["i8016_03c", "mfissile"])
+def test_plotlegends(plut, name):
+    from matplotlib import pyplot
+
+    pyplot.close("all")
+
+    ax = pyplot.gca()
+    case = plut[name]
+    # Plot all MTs
+    case.plot(ax=ax)
+    assert getLegendTexts(ax) == case.MTdescrip
+    pyplot.cla()
+
+    case.plot(case.MT[:2], ax=ax)
+    assert getLegendTexts(ax) == case.MTdescrip[:2]
+    pyplot.cla()
+
+    case.plot(case.MT[:2], ax=ax, labels=["foo", "bar"])
+    assert getLegendTexts(ax) == ["foo", "bar"]
+    pyplot.cla()
+
+    # Try a dictionary, but see if the plotter falls back for
+    # missing labels
+
+    case.plot(case.MT[:2], ax=ax, labels={case.MT[1]: "baz"})
+    assert getLegendTexts(ax) == [case.MTdescrip[0], "baz"]
+
+    with pytest.raises(ValueError):
+        case.plot(labels=["hello", "world"], label=["failure", "mode"])
