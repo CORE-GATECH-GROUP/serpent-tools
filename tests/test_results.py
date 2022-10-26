cdf7e2844a3b2b63cc684139f3a348a5439e01ab
Andrew Johnson <drewejohnson@users.noreply.github.com>
2022-10-26 15:16:27 -0700

    ENH ResultsReader supports Serpent 2.2.0
    Closes #467

--- tests/test_results.py
+++ tests/test_results.py
@@ -2,10 +2,13 @@ import re
 import os
 from os.path import join as pjoin
 from tempfile import TemporaryDirectory
+from pathlib import Path
 
 import numpy
 import pytest
 import serpentTools
+
+from . import LoggerMixin
 
 
 @pytest.fixture
@@ -55,8 +58,8 @@ def fake2132File(tdir):
     newfile = pjoin(tdir, "InnerAssembly_2132_res.m")
     with open(newfile, "w") as ostream, open(basefile, "r") as istream:
         for line in istream:
-            if "2.1.31" in line:
-                ostream.write(line.replace("2.1.31", "2.1.32"))
+            if "2.1.30" in line:
+                ostream.write(line.replace("2.1.30", "2.1.32"))
             else:
                 ostream.write(line)
             if line.startswith("BURN_STEP"):
@@ -90,3 +93,33 @@ def test_2132_filterburnup(fake2132File):
     assert "burnDays" in reader.resdata
     assert "absKeff" in reader.resdata
     assert "pop" not in reader.metadata
+
+
+@pytest.fixture
+def fake220File(tmp_path: Path) -> str:
+    fname = "InnerAssembly_res.m"
+    originalFile = serpentTools.data.getFile(fname)
+    newFile = tmp_path / fname
+
+    with open(originalFile) as orig, newFile.open("w") as wstream:
+        for line in orig:
+            if "2.1.30" not in line:
+                wstream.write(line)
+            else:
+                wstream.write(line.replace("2.1.30", "2.2.0"))
+    yield str(newFile)
+
+
+@pytest.fixture
+def logInterceptor() -> LoggerMixin:
+    logger = LoggerMixin()
+    logger.attach()
+    yield logger
+    logger.detach()
+
+
+def test_nowarns_220(fake220File: str, logInterceptor: LoggerMixin):
+    with serpentTools.settings.rc as rc:
+        rc["serpentVersion"] = "2.2.0"
+        serpentTools.read(fake220File, "results")
+    assert not logInterceptor.handler.logMessages
