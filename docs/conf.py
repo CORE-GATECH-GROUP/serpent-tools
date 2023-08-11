77e1364057eca30c48d9a269282fa3387e66c560
Andrew Johnson <drewejohnson@users.noreply.github.com>
2023-08-11 16:28:33 -0700

    DOC: Explicit english language parameter
    Silences a sphinx doc warning

--- docs/conf.py
+++ docs/conf.py
@@ -72,7 +72,7 @@ del devTeamLink
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "english"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
