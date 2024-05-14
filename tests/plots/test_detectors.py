6a0539dce60652d5bc7b58efa672c6d0ba69786e
Andrew Johnson <drewejohnson@users.noreply.github.com>
2024-05-14 14:39:14 -0700

    Add hex detector plot test

--- tests/plots/test_detectors.py
+++ tests/plots/test_detectors.py
@@ -20,3 +20,12 @@ def testBwrMeshPlot(recwarn):
 def testSpectrumJustNormPerLethargy():
     reader = readDataFile("bwr_det0.m")
     reader["spectrum"].spectrumPlot(by=None, fixed={"reaction": 1})
+
+
+@compare_or_update_plot
+def testHexPlot():
+    reader = readDataFile("hexplot_det0.m")
+    h2 = reader["hex2"]
+    h2.pitch = 1
+    h2.hexType = 2
+    h2.hexPlot()
